1ceac3ed02793a9578c55c85b006b10a5cc0ae87
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2022-11-04 20:35:14 +0300

    baseurl try again

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,8 +1,8 @@
-// const baseUrl = process.env.NODE_ENV === "production" 
-// ? 'next-custom-server-sequilizee.vercel.app' 
-// : 'http://localhost:3000';
-// export default baseUrl;
-
-
-const baseUrl ='http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === "production" 
+? 'next-custom-server-sequilizee.vercel.app' 
+: 'http://localhost:3000';
 export default baseUrl;
+
+
+//const baseUrl ='http://localhost:3000';
+//export default baseUrl;
